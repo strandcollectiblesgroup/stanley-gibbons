7b0aa91ef92af290b15d10afb19722dbb4273030
JP Nothard <jpnothard@gmail.com>
2024-05-21 14:15:53 +0100

    chore: updated the vite config

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -5,4 +5,11 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['sanity/router']
+      }
+    }
+  }
 })
