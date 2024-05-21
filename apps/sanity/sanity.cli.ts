27db6e6ddce9ef05c41e972162c483b7ad6f2328
JP Nothard <jpnothard@gmail.com>
2024-05-21 14:46:14 +0100

    chore: removed rollup

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -5,11 +5,4 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ['sanity/router']
-      }
-    }
-  }
 })
